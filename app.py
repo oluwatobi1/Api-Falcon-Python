174dbde93454de9dc1fa46d368dfc4474ee4f231
oluwatobi1 <akintunlesetobi@gmail.com>
2021-12-11 11:29:13 +0100

    crud operation with falcon

--- app.py
+++ app.py
@@ -1,19 +1,56 @@
 import json, falcon
-
-
-class ItemDetails:
+from wsgiref.simple_server import make_server
+class Items:
     def on_get(self,req, resp):
-        content = ("You have hit this endpoint successfully")
+        resp.status = falcon.HTTP_200
+        raw_data = req.stream.read()
+        if raw_data:
+            data = json.loads(raw_data)
+        else:
+            data={'name':"Unknown"}
+
+        content = {"messge": "Hello {a}, You have hit this endpoint successfully.".format(a=data['name'])}
         resp.body = json.dumps(content)
+
+
+    def on_post(self, req, resp):
+        resp.status = falcon.HTTP_200
+
+        data = json.loads(req.stream.read())
+        if data:
+            ans = int(data.get('x'))+ int(data.get('y'))
+            output = {'message': '{a} plus {b} equals {ans}.'.format(ans=ans, a=data["x"], b=data['y'])}
+        else:
+            ans = None
+            output = {"message": "Invalid input: missing value for (x), (y)"}
+
+        resp.body = json.dumps(output)
+
+    def on_put(self, req, resp):
+        resp.status = falcon.HTTP_200
+        output = {"message": "Hey... Put method not supported"}
+        resp.body = json.dumps(output)
+
+    def on_delete(self, req, resp):
+        resp.status = falcon.HTTP_200
+        output = {"message": "Hey... Delete method not supported"}
+        resp.body = json.dumps(output)
+
+    
+
+
 
 
 
 app = falcon.App()
 
-item = ItemDetails()
+item = Items()
 app.add_route('/item', item)
+
 
 # if __name__ == '__main__':
 #     with make_server('', 8000, app) as httpd:
-#         print("Serving port at 8000")
+#         print('Serving on port 8000...')
+
+#         # Serve until process is killed
 #         httpd.serve_forever()
